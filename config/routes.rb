adcc4cb80360aa35aab1f74b3448826fabc88590
Le Anh Viet <leanhviet24495@gmail.com>
2017-11-10 00:57:00 +0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  get  'static_pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'static_pages#home'
 
 
-  root 'application#hello'
+#
 end
